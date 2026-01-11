0046b4d9345308d644f32b72378fb7bb17200cca
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-01-11 18:03:16 -0500

    feat: Add DTOs ProductoCarrito, Carrito Request and Response for Carrito

---
+++ app/src/main/java/com/thekingmoss/dto/carrito/CarritoRequestDto.kt
@@ -0,0 +1,6 @@
+package com.thekingmoss.dto.carrito
+
+data class CarritoRequestDto (
+    val productoId: Long,
+    val cantidad: Int
+)
---
+++ app/src/main/java/com/thekingmoss/dto/carrito/ProductoCarritoDto.kt
@@ -0,0 +1,9 @@
+package com.thekingmoss.dto.carrito
+
+data class ProductoCarritoDto(
+    val productoId: Long,
+    val nombreProducto: String,
+    val imagenUrl: String?,
+    val precioUnitario: Double,
+    val stock: Int
+)
