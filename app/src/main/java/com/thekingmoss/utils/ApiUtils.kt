906f5c20309846ad60ec81d31a613d1c1740ca03
Daniel-JA03 <danieljaimesamancio@gmail.com>
2026-01-11 21:32:31 -0500

    feat: add RetrofitClient and ApiUtils

---
+++ app/src/main/java/com/thekingmoss/utils/ApiUtils.kt
@@ -0,0 +1,15 @@
+package com.thekingmoss.utils
+
+import com.thekingmoss.services.ApiServiceCarrito
+
+class ApiUtils {
+    companion object {
+        val BASE_URL = "http://192.168.1.108:8091/"
+
+        fun carritoService(token: String): ApiServiceCarrito {
+            return RetrofitClient
+                .getClient(BASE_URL, token)
+                .create(ApiServiceCarrito::class.java)
+        }
+    }
+}
